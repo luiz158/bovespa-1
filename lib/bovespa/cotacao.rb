937da084cf057b4a917e0d3862a06e7722da60ca
Marco Antonio <marcofognog@gmail.com>
2012-02-25 17:10:15 -0300

    Making code more easy to read by applying some code standards.

--- lib/bovespa/cotacao.rb
+++ lib/bovespa/cotacao.rb
@@ -6,26 +6,25 @@ module Bovespa
 		attr_accessor :raw_attributes
 
 		def initialize(nome_do_ativo)
-			uri = URI.parse("http://www.bmfbovespa.com.br/cotacoes2000/formCotacoesMobile.asp?codsocemi="+nome_do_ativo)
+			uri = URI.parse("http://www.bmfbovespa.com.br/cotacoes2000/formCotacoesMobile.asp?codsocemi=" + nome_do_ativo)
 			res = Net::HTTP.start(uri.host, uri.port) do |http|
-				http.get("/cotacoes2000/formCotacoesMobile.asp?codsocemi="+nome_do_ativo)
+				http.get("/cotacoes2000/formCotacoesMobile.asp?codsocemi=" + nome_do_ativo)
 			end
 			xml = Nokogiri::XML(res.body)
 			raw_attr = {}
 			xml.css("PAPEL").each do |node|
-						@raw_attributes = node
+				@raw_attributes = node
 			end
 			create_attributes
 		end
 
 		def create_attributes
-			attr_list = ["codigo", "delay", "data", "hora", "oscilacao", "valor_ultimo", "quant_neg", "mercado","descricao"]
+			attr_list = ["codigo", "delay", "data", "hora", "oscilacao", "valor_ultimo", "quant_neg", "mercado", "descricao"]
 			attr_list.each do |method_name|
 				self.instance_eval("def #{method_name}; @raw_attributes['#{method_name.upcase}']; end")
 			end
 		end
 
-	# more attributes
 		def ultimo_valor
 			valor_ultimo
 		end
@@ -37,5 +36,6 @@ module Bovespa
 				return false
 			end
 		end
+
 	end
 end
